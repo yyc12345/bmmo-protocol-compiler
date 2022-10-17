9062e8060c4d325df0c0ab5e81bab0c80c407798
yyc12345 <yyc12321@outlook.com>
2022-10-17 16:16:59 +0800

    fulfill fs function decls and copy boost fs test data

--- src/bpc_fs.h
+++ src/bpc_fs.h
@@ -11,7 +11,27 @@
 #endif
 
 gchar* bpcfs_vsprintf(const char* format, va_list ap);
+
 void bpcfs_write_snippets(FILE* fs, BPCSNP_EMBEDDED_FILE* snp);
 FILE* bpcfs_fopen_glibfs(const gchar* glibfs_filepath, bool is_open);
-gchar* bpcfs_replace_ext(gchar* u8_path, const gchar* u8_ext_with_dot);
-gchar* bpcfs_simple_relative_path(const gchar* u8_this, const gchar* u8_base);
+
+// Ref: https://github.com/boostorg/filesystem/blob/9613ccfa4a2c47bbc7059bf61dd52aec11e53893/src/path.cpp#L78
+bool _bpcfs_is_device_name_char(gchar single);
+// Ref: https://github.com/boostorg/filesystem/blob/9613ccfa4a2c47bbc7059bf61dd52aec11e53893/src/path.cpp#L114
+size_t _bpcfs_find_separator(const gchar* in_u8path, size_t size);
+// Ref: https://github.com/boostorg/filesystem/blob/9613ccfa4a2c47bbc7059bf61dd52aec11e53893/src/path.cpp#L831
+size_t _bpcfs_find_root_directory_start(const gchar* in_u8path, size_t size);
+// Ref: https://github.com/boostorg/filesystem/blob/9613ccfa4a2c47bbc7059bf61dd52aec11e53893/src/path.cpp#L811
+size_t _bpcfs_find_filename_size(const gchar* in_u8path, size_t root_name_size, size_t end_pos);
+// Ref: https://github.com/boostorg/filesystem/blob/cffb1d1bbdac53e308d489599138d03fd43f6cbf/include/boost/filesystem/path.hpp#L1347
+bool _bpcfs_is_directory_separator(gchar single);
+// Ref: https://github.com/boostorg/filesystem/blob/9613ccfa4a2c47bbc7059bf61dd52aec11e53893/src/path.cpp#L797
+bool _bpcfs_is_root_separator(const gchar* in_u8path, size_t root_dir_pos, size_t pos);
+// Ref: https://github.com/boostorg/filesystem/blob/9613ccfa4a2c47bbc7059bf61dd52aec11e53893/src/path.cpp#L434
+gchar* _bpcfs_filename(const gchar* u8_path);
+// Ref: https://github.com/boostorg/filesystem/blob/9613ccfa4a2c47bbc7059bf61dd52aec11e53893/src/path.cpp#L496
+gchar* _bpcfs_extension(gchar* u8_path);
+// Ref: https://github.com/boostorg/filesystem/blob/9613ccfa4a2c47bbc7059bf61dd52aec11e53893/src/path.cpp#L312
+gchar* bpcfs_replace_extension(const gchar* u8_path, const gchar* u8_ext);
+// Ref: https://github.com/boostorg/filesystem/blob/9613ccfa4a2c47bbc7059bf61dd52aec11e53893/src/path.cpp#L551
+gchar* bpcfs_lexically_relative(const gchar* u8_this, const gchar* u8_base);
--- tests/tests_fs.c
+++ tests/tests_fs.c
@@ -1,5 +1,131 @@
 #include "main.h"
+#include "../src/bpc_fs.h"
+
+typedef struct _BPCDB_FS_REPLACE_EXTENSION {
+	gchar* in_origin;
+	gchar* in_ext;
+	gchar* out_expect;
+}BPCDB_FS_REPLACE_EXTENSION;
+// Ref: https://github.com/boostorg/filesystem/blob/5d4c1caaab65012f939ffa118cc1f36d34198c0b/test/path_test.cpp#L2466
+static const BPCDB_FS_REPLACE_EXTENSION dbset_replace_extension[] = {
+	{ "", "", "" },
+    { "", "a", ".a" },
+    { "", "a.", ".a." },
+    { "", ".a", ".a" },
+    { "", "a.txt", ".a.txt" },
+    // see the rationale in html docs for explanation why this works:
+    { "", ".txt", ".txt" },
+
+    { "a.txt", "", "a" },
+    { "a.txt", "", "a" },
+    { "a.txt", ".", "a." },
+    { "a.txt", ".tex", "a.tex" },
+    { "a.txt", "tex", "a.tex" },
+    { "a.", ".tex", "a.tex" },
+    { "a.", "tex", "a.tex" },
+    { "a", ".txt", "a.txt" },
+    { "a", "txt", "a.txt" },
+    { "a.b.txt", ".tex", "a.b.tex" },
+    { "a.b.txt", "tex", "a.b.tex" },
+    { "a/b", ".c", "a/b.c" },
+    { "a.txt/b", ".c", "a.txt/b.c" },         // ticket 4702
+    { "foo.txt", "exe", "foo.exe" },          // ticket 5118
+    { "foo.txt", ".tar.bz2", "foo.tar.bz2" } // ticket 5118
+};
+static const size_t dblen_replace_extension = sizeof(dbset_replace_extension) / sizeof(BPCDB_FS_REPLACE_EXTENSION);
+
+static void _bpctest_fs_replace_extension(gconstpointer rawdata) {
+	const BPCDB_FS_REPLACE_EXTENSION* data = (BPCDB_FS_REPLACE_EXTENSION*)rawdata;
+	gchar* result = bpcfs_replace_extension(data->in_origin, data->in_ext);
+	g_assert_cmpstr(result, ==, data->out_expect);
+	g_free(result);
+}
+static void bpctest_fs_replace_extension() {
+	BPCTEST_TEST_DATASET(
+		"/fs/replace_extension", dbset_replace_extension, dblen_replace_extension,
+		_bpctest_fs_replace_extension);
+}
+
+typedef struct _BPCDB_FS_LEXICALLY_RELATIVE {
+	gchar* in_this;
+	gchar* in_base;
+	gchar* out_relative;
+}BPCDB_FS_LEXICALLY_RELATIVE;
+// Ref: https://github.com/boostorg/filesystem/blob/4319cf13887aaf5892e0b36c4eab99424277ebaa/test/relative_test.cpp#L27
+static const BPCDB_FS_LEXICALLY_RELATIVE dbset_lexically_relative[] = {
+	{ "", "", "" },
+    { "", "/foo", "" },
+    { "/foo", "", "" },
+    { "/foo", "/foo", "." },
+    { "", "foo", "" },
+    { "foo", "", "" },
+    { "foo", "foo", "." },
+
+    { "a/b/c", "a", "b/c" },
+    { "a//b//c", "a", "b/c" },
+    { "a/b/c", "a/b", "c" },
+    { "a///b//c", "a//b", "c" },
+    { "a/b/c", "a/b/c", "." },
+    { "a/b/c", "a/b/c/x", ".." },
+    { "a/b/c", "a/b/c/x/y", "../.." },
+    { "a/b/c", "a/x", "../b/c" },
+    { "a/b/c", "a/b/x", "../c" },
+    { "a/b/c", "a/x/y", "../../b/c" },
+    { "a/b/c", "a/b/x/y", "../../c" },
+    { "a/b/c", "a/b/c/x/y/z", "../../.." },
+    { "a/b/c", "a/", "b/c" },
+    { "a/b/c", "a/.", "b/c" },
+    { "a/b/c", "a/./", "b/c" },
+    { "a/b/c", "a/b/..", "" },
+    { "a/b/c", "a/b/../", "" },
+    { "a/b/c", "a/b/d/..", "c" },
+    { "a/b/c", "a/b/d/../", "c" },
+
+    // paths unrelated except first element, and first element is root directory
+    { "/a/b/c", "/x", "../a/b/c" },
+    { "/a/b/c", "/x/y", "../../a/b/c" },
+    { "/a/b/c", "/x/y/z", "../../../a/b/c" },
+
+    // paths unrelated
+    { "a/b/c", "x", "" },
+    { "a/b/c", "x/y", "" },
+    { "a/b/c", "x/y/z", "" },
+    { "a/b/c", "/x", "" },
+    { "a/b/c", "/x/y", "" },
+    { "a/b/c", "/x/y/z", "" },
+    { "a/b/c", "/a/b/c", "" },
+
+    // TODO: add some Windows-only test cases that probe presence or absence of
+    // drive specifier-and root-directory
+
+    //  Some tests from Jamie Allsop's paper
+    { "/a/d", "/a/b/c", "../../d" },
+    { "/a/b/c", "/a/d", "../b/c" },
+#ifdef G_OS_WIN32
+    { "c:\\y", "c:\\x", "../y" },
+#else
+    { "c:\\y", "c:\\x", "" },
+#endif
+    { "d:\\y", "c:\\x", "" },
+
+    //  From issue #1976
+    { "/foo/new", "/foo/bar", "../new" }
+};
+static const size_t dblen_lexically_relative = sizeof(dbset_lexically_relative) / sizeof(BPCDB_FS_LEXICALLY_RELATIVE);
+
+static void _bpctest_fs_lexically_relative(gconstpointer rawdata) {
+	const BPCDB_FS_LEXICALLY_RELATIVE* data = (BPCDB_FS_LEXICALLY_RELATIVE*)rawdata;
+	gchar* result = bpcfs_lexically_relative(data->in_this, data->in_base);
+	g_assert_cmpstr(result, ==, data->out_relative);
+	g_free(result);
+};
+static void bpctest_fs_lexically_relative() {
+	BPCTEST_TEST_DATASET(
+		"/fs/lexically_relative", dbset_lexically_relative, dblen_lexically_relative,
+		_bpctest_fs_lexically_relative);
+}
 
 void bpctest_fs() {
-
+	bpctest_fs_replace_extension();
+	bpctest_fs_lexically_relative();
 }
