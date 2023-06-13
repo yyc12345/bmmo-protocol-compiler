0a2845162ee8c48fb28ac54c0a18c1205bab3232
yyc12345 <yyc12321@outlook.com>
2023-06-13 21:51:11 +0800

    [feat] update csharp template. optimize functions. the ordered min ver updated to .net core 2.1

--- snippets/functions.cs
+++ snippets/functions.cs
@@ -6,7 +6,7 @@ public abstract class BpStruct {
 
 public abstract class BpMessage : BpStruct {
     public virtual OpCode GetOpCode() { throw new NotImplementedException(); }
-    public virtual bool GetIsReliable() { throw new NotImplementedException(); }
+    public virtual bool IsReliable() { throw new NotImplementedException(); }
 }
 
 public static partial class BPHelper {
@@ -15,93 +15,103 @@ public static partial class BPHelper {
         instance.Serialize(bw);
     }
 
-    public static TInt[] _CastEnumArray2IntArray<TEnum, TInt>(TEnum[] d) where TInt : struct
-        where TEnum : Enum => d.Cast<TInt>().ToArray();
-    public static TEnum[] _CastIntArray2EnumArray<TEnum, TInt>(TInt[] d) where TInt : struct
-        where TEnum : Enum => d.Cast<TEnum>().ToArray();
+    private static void _BpByteSwap(Span<byte> data, int unit_size) {
+        switch (unit_size) {
+            case 1:
+                return;     // byte do not need any swap
+            case 2: 
+                {
+                    var span16 = MemoryMarshal.Cast<byte, ushort>(data);
+                    for (int i = 0; i < span16.Length; ++i) {
+                        span16[i] = BinaryPrimitives.ReverseEndianness(span16[i]);
+                    }
+                    break;
+                }
+            case 4: {
+                    var span32 = MemoryMarshal.Cast<byte, uint>(data);
+                    for (int i = 0; i < span32.Length; ++i) {
+                        span32[i] = BinaryPrimitives.ReverseEndianness(span32[i]);
+                    }
+                    break;
+                }
+            case 8: {
+                    var span64 = MemoryMarshal.Cast<byte, ulong>(data);
+                    for (int i = 0; i < span64.Length; ++i) {
+                        span64[i] = BinaryPrimitives.ReverseEndianness(span64[i]);
+                    }
+                    break;
+                }
+            default:
+                throw new ArgumentOutOfRangeException("Illegal unit size for byte swap!");
+        }
+    }
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static byte _BpReadByte(this BinaryReader br) => br.ReadByte();
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static sbyte _BpReadSByte(this BinaryReader br) => br.ReadSByte();
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static UInt16 _BpReadUInt16(this BinaryReader br) => br.ReadUInt16();
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static Int16 _BpReadInt16(this BinaryReader br) => br.ReadInt16();
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static UInt32 _BpReadUInt32(this BinaryReader br) => br.ReadUInt32();
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static Int32 _BpReadInt32(this BinaryReader br) => br.ReadInt32();
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static UInt64 _BpReadUInt64(this BinaryReader br) => br.ReadUInt64();
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static Int64 _BpReadInt64(this BinaryReader br) => br.ReadInt64();
+
+    public static void _BpSkip(this BinaryReader br, long offset) {
+        br.BaseStream.Seek(offset, SeekOrigin.Current);
+    }
 
     public static string _BpReadString(this BinaryReader br) {
         UInt32 length = br._BpReadUInt32();
         return Encoding.UTF8.GetString(br.ReadBytes((int)length));
     }
 
-    public static Span<TInt> _BpReadAbstractIntArray<TInt>(this BinaryReader br, int count, int int_size) where TInt : struct {
-        Span<byte> raw_span = br.ReadBytes(count * int_size).AsSpan();
-        return MemoryMarshal.Cast<byte, TInt>(raw_span);
+    public static void _BpReadNumberTuple<TItem>(this BinaryReader br, ref TItem[] data, int unit_size) where TItem : struct {
+        var buffer = MemoryMarshal.Cast<TItem, byte>(data.AsSpan());
+        br.Read(buffer);
+        if (!BitConverter.IsLittleEndian) _BpByteSwap(buffer, unit_size);
     }
-    public static byte[] _BpReadByteArray(this BinaryReader br, int count) {
-        return br.ReadBytes(count);
-    }
-    public static sbyte[] _BpReadSByteArray(this BinaryReader br, int count) {
-        return br._BpReadAbstractIntArray<sbyte>(count, sizeof(sbyte)).ToArray();
-    }
-    public static UInt16[] _BpReadUInt16Array(this BinaryReader br, int count) {
-        Span<UInt16> endian_span = br._BpReadAbstractIntArray<UInt16>(count, sizeof(ushort));
-        if (!BitConverter.IsLittleEndian)
-            for (int i = 0; i < endian_span.Length; ++i) {
-                endian_span[i] = BinaryPrimitives.ReverseEndianness(endian_span[i]);
-            }
-        return endian_span.ToArray();
-    }
-    public static Int16[] _BpReadInt16Array(this BinaryReader br, int count) {
-        Span<Int16> endian_span = br._BpReadAbstractIntArray<Int16>(count, sizeof(short));
-        if (!BitConverter.IsLittleEndian)
-            for (int i = 0; i < endian_span.Length; ++i) {
-                endian_span[i] = BinaryPrimitives.ReverseEndianness(endian_span[i]);
-            }
-        return endian_span.ToArray();
-    }
-    public static UInt32[] _BpReadUInt32Array(this BinaryReader br, int count) {
-        Span<UInt32> endian_span = br._BpReadAbstractIntArray<UInt32>(count, sizeof(uint));
-        if (!BitConverter.IsLittleEndian)
-            for (int i = 0; i < endian_span.Length; ++i) {
-                endian_span[i] = BinaryPrimitives.ReverseEndianness(endian_span[i]);
-            }
-        return endian_span.ToArray();
-    }
-    public static Int32[] _BpReadInt32Array(this BinaryReader br, int count) {
-        Span<Int32> endian_span = br._BpReadAbstractIntArray<Int32>(count, sizeof(int));
-        if (!BitConverter.IsLittleEndian)
-            for (int i = 0; i < endian_span.Length; ++i) {
-                endian_span[i] = BinaryPrimitives.ReverseEndianness(endian_span[i]);
-            }
-        return endian_span.ToArray();
-    }
-    public static UInt64[] _BpReadUInt64Array(this BinaryReader br, int count) {
-        Span<UInt64> endian_span = br._BpReadAbstractIntArray<UInt64>(count, sizeof(ulong));
-        if (!BitConverter.IsLittleEndian)
-            for (int i = 0; i < endian_span.Length; ++i) {
-                endian_span[i] = BinaryPrimitives.ReverseEndianness(endian_span[i]);
-            }
-        return endian_span.ToArray();
-    }
-    public static Int64[] _BpReadInt64Array(this BinaryReader br, int count) {
-        Span<Int64> endian_span = br._BpReadAbstractIntArray<Int64>(count, sizeof(long));
-        if (!BitConverter.IsLittleEndian)
-            for (int i = 0; i < endian_span.Length; ++i) {
-                endian_span[i] = BinaryPrimitives.ReverseEndianness(endian_span[i]);
-            }
-        return endian_span.ToArray();
+
+    public static void _BpReadNumberList<TItem>(this BinaryReader br, ref List<TItem> data, int count, int unit_size) where TItem : struct {
+        var raw = new TItem[count];
+
+        var buffer = MemoryMarshal.Cast<TItem, byte>(raw);
+        br.Read(buffer);
+        if (!BitConverter.IsLittleEndian) _BpByteSwap(buffer, unit_size);
+
+        data.Clear();
+        data.AddRange(raw);
     }
 
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static void _BpWriteByte(this BinaryWriter bw, byte data) => bw.Write(data);
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static void _BpWriteSByte(this BinaryWriter bw, sbyte data) => bw.Write(data);
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static void _BpWriteUInt16(this BinaryWriter bw, UInt16 data) => bw.Write(data);
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static void _BpWriteInt16(this BinaryWriter bw, Int16 data) => bw.Write(data);
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static void _BpWriteUInt32(this BinaryWriter bw, UInt32 data) => bw.Write(data);
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static void _BpWriteInt32(this BinaryWriter bw, Int32 data) => bw.Write(data);
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static void _BpWriteUInt64(this BinaryWriter bw, UInt64 data) => bw.Write(data);
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static void _BpWriteInt64(this BinaryWriter bw, Int64 data) => bw.Write(data);
+
+    public static void _BpSkip(this BinaryWriter bw, long offset) {
+        if (offset == 0) return;
+        bw.BaseStream.Seek(offset - 1, SeekOrigin.Current);
+        bw.Write((byte)0);
+    }
 
     public static void _BpWriteString(this BinaryWriter bw, string strl) {
         var rawstr = Encoding.UTF8.GetBytes(strl);
@@ -110,102 +120,22 @@ public static partial class BPHelper {
         bw.Write(rawstr);
     }
 
-    public static void _BpWriteAbstractIntArray<TInt>(this BinaryWriter bw, Span<TInt> endian_span) where TInt : struct {
-        Span<byte> raw_span = MemoryMarshal.Cast<TInt, byte>(endian_span);
-#if NETCOREAPP2_1_OR_GREATER
-        bw.Write(raw_span);     // .net core 2.1 and above support directly write.
-#else
-        bw.Write(raw_span.ToArray());
-#endif
-    }
-    public static void _BpWriteByteArray(this BinaryWriter bw, byte[] data) {
-        bw.Write(data);
-    }
-    public static void _BpWriteSByteArray(this BinaryWriter bw, sbyte[] data) {
-        bw._BpWriteAbstractIntArray<sbyte>(data.AsSpan());
-    }
-    public static void _BpWriteUInt16Array(this BinaryWriter bw, UInt16[] data) {
-        Span<UInt16> endian_span;
+
+    public static void _BpWriteNumberTuple<TItem>(this BinaryWriter bw, ref TItem[] data, int unit_size) where TItem : struct {
+        Span<byte> buffer;
         if (!BitConverter.IsLittleEndian) {
-            endian_span = new Span<UInt16>(new UInt16[data.Length]);
-            data.AsSpan().CopyTo(endian_span);
-            for (int i = 0; i < endian_span.Length; ++i) {
-                endian_span[i] = BinaryPrimitives.ReverseEndianness(endian_span[i]);
-            }
+            buffer = MemoryMarshal.Cast<TItem, byte>(data.AsSpan());
         } else {
-            endian_span = data.AsSpan();
+            var intermediary = (TItem[])data.Clone();
+            buffer = MemoryMarshal.Cast<TItem, byte>(intermediary.AsSpan());
+            _BpByteSwap(buffer, unit_size);
         }
 
-        bw._BpWriteAbstractIntArray<UInt16>(endian_span);
+        bw.Write(buffer);
     }
-    public static void _BpWriteInt16Array(this BinaryWriter bw, Int16[] data) {
-        Span<Int16> endian_span;
-        if (!BitConverter.IsLittleEndian) {
-            endian_span = new Span<Int16>(new Int16[data.Length]);
-            data.AsSpan().CopyTo(endian_span);
-            for (int i = 0; i < endian_span.Length; ++i) {
-                endian_span[i] = BinaryPrimitives.ReverseEndianness(endian_span[i]);
-            }
-        } else {
-            endian_span = data.AsSpan();
-        }
 
-        bw._BpWriteAbstractIntArray<Int16>(endian_span);
+    public static void _BpWriteNumberList<TItem>(this BinaryWriter bw, ref List<TItem> data, int unit_size) where TItem : struct {
+        var buffer = MemoryMarshal.Cast<TItem, byte>(data.ToArray().AsSpan());
+        if (!BitConverter.IsLittleEndian) _BpByteSwap(buffer, unit_size);
+        bw.Write(buffer);
     }
-    public static void _BpWriteUInt32Array(this BinaryWriter bw, UInt32[] data) {
-        Span<UInt32> endian_span;
-        if (!BitConverter.IsLittleEndian) {
-            endian_span = new Span<UInt32>(new UInt32[data.Length]);
-            data.AsSpan().CopyTo(endian_span);
-            for (int i = 0; i < endian_span.Length; ++i) {
-                endian_span[i] = BinaryPrimitives.ReverseEndianness(endian_span[i]);
-            }
-        } else {
-            endian_span = data.AsSpan();
-        }
-
-        bw._BpWriteAbstractIntArray<UInt32>(endian_span);
-    }
-    public static void _BpWriteInt32Array(this BinaryWriter bw, Int32[] data) {
-        Span<Int32> endian_span;
-        if (!BitConverter.IsLittleEndian) {
-            endian_span = new Span<Int32>(new Int32[data.Length]);
-            data.AsSpan().CopyTo(endian_span);
-            for (int i = 0; i < endian_span.Length; ++i) {
-                endian_span[i] = BinaryPrimitives.ReverseEndianness(endian_span[i]);
-            }
-        } else {
-            endian_span = data.AsSpan();
-        }
-
-        bw._BpWriteAbstractIntArray<Int32>(endian_span);
-    }
-    public static void _BpWriteUInt64Array(this BinaryWriter bw, UInt64[] data) {
-        Span<UInt64> endian_span;
-        if (!BitConverter.IsLittleEndian) {
-            endian_span = new Span<UInt64>(new UInt64[data.Length]);
-            data.AsSpan().CopyTo(endian_span);
-            for (int i = 0; i < endian_span.Length; ++i) {
-                endian_span[i] = BinaryPrimitives.ReverseEndianness(endian_span[i]);
-            }
-        } else {
-            endian_span = data.AsSpan();
-        }
-
-        bw._BpWriteAbstractIntArray<UInt64>(endian_span);
-    }
-    public static void _BpWriteInt64Array(this BinaryWriter bw, Int64[] data) {
-        Span<Int64> endian_span;
-        if (!BitConverter.IsLittleEndian) {
-            endian_span = new Span<Int64>(new Int64[data.Length]);
-            data.AsSpan().CopyTo(endian_span);
-            for (int i = 0; i < endian_span.Length; ++i) {
-                endian_span[i] = BinaryPrimitives.ReverseEndianness(endian_span[i]);
-            }
-        } else {
-            endian_span = data.AsSpan();
-        }
-
-        bw._BpWriteAbstractIntArray<Int64>(endian_span);
-    }
-}
